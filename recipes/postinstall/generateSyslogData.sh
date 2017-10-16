a0ef06289dadeb3b7d34011d4ece4001c795db12
oascofare <oascofare@users.noreply.github.com>
2017-10-16 16:15:49 -0400

    upate gen script

--- recipes/postinstall/generateSyslogData.sh
+++ recipes/postinstall/generateSyslogData.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 echo "*********************************Install and start syslog analytic demo simulation data *****************************"
+yum install -y wget
+yum install -y git
 git clone https://github.com/oascofare/Hackathon2017
 cd Hackathon2017
 ./shell/configRsyslog.sh
