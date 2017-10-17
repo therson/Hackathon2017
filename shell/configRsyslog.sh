764614fb27352e3d66ee2a2eff89df25e6c72291
oascofare <omar.ascofare@gmail.com>
2017-10-17 12:02:26 -0400

    config update

--- shell/configRsyslog.sh
+++ shell/configRsyslog.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
+ 
+getNifiHost () {
+       	NIFI_HOST=$(curl -u admin:admin -X GET http://$AMBARI_HOST:8080/api/v1/clusters/$CLUSTER_NAME/services/NIFI/components/NIFI_MASTER|grep "host_name"|grep -Po ': "([a-zA-Z0-9\-_!?.]+)'|grep -Po '([a-zA-Z0-9\-_!?.]+)')
 
-yum install rsyslog
+       	echo $NIFI_HOST
+}
 
-echo "*.* @127.0.0.1:7780" >> /etc/rsyslog.conf 
+export NIFI_HOST=$(getNifiHost)
+
+sleep 2
+       	echo "*********************************Creating RSYSLOG  configuration...  "
+  
+        echo "*.* @$NIFI_HOST:7780" >> /etc/rsyslog.conf 
+
+        service rsyslog restart
 
 service rsyslog restart
