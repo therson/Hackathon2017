676027570d9a57f68c7a8a159dd39d0fac93870b
oascofare <omar.ascofare@gmail.com>
2017-10-17 14:31:34 -0400

    updates

--- recipes/postinstall/machine-log-demo-Install.sh
+++ recipes/postinstall/machine-log-demo-Install.sh
@@ -2,6 +2,14 @@
 echo "*********************************Install Machine log demo data *****************************"
 yum install -y wget
 yum install -y git
+
+git clone https://github.com/vakshorton/CloudBreakArtifacts
+
+echo "************ Installing HDF components ************************"
+chmod +x -R ./CloudBreakArtifacts/recipes/
+./CloudBreakArtifacts/recipes/hdp-hdf-post-install.sh
+
+echo "************ Installing Demo  ************************"
 git clone https://github.com/oascofare/Hackathon2017
 cd Hackathon2017
 chmod +x -R ./shell
