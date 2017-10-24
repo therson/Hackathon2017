835cd7bfa3936abe4cd413c7a19126f360a7c21d
zoharsan <zoharsan@gmail.com>
2017-10-24 10:18:49 -0400

    Update configrsyslog.sh
    Set environment variables AMBARI_HOST and CLUSTER_NAME

--- shell/configRsyslog.sh
+++ shell/configRsyslog.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
- 
+
+export AMBARI_HOST=$(hostname -f)
+export CLUSTER_NAME=$(curl -u admin:admin -X GET http://$AMBARI_HOST:8080/api/v1/clusters |grep cluster_name|grep -Po ': "(.+)'|grep -Po '[a-zA-Z0-9\-_!?.]+')
+
 getNifiHost () {
        	NIFI_HOST=$(curl -u admin:admin -X GET http://$AMBARI_HOST:8080/api/v1/clusters/$CLUSTER_NAME/services/NIFI/components/NIFI_MASTER|grep "host_name"|grep -Po ': "([a-zA-Z0-9\-_!?.]+)'|grep -Po '([a-zA-Z0-9\-_!?.]+)')
 
