a5f3f30abf594940599175e4a72a6542772f9b03
mujiqadri <mujiqadri@users.noreply.github.com>
2017-10-24 16:32:30 -0400

    Update deployZeppelinNotebook.sh

--- shell/deployZeppelinNotebook.sh
+++ shell/deployZeppelinNotebook.sh
@@ -1,36 +1,38 @@
-
 #!/bin/bash
 
 export AMBARI_HOST=$(hostname -f)
 export ROOTPATH='~'
 export CLUSTER_NAME=$(curl -u admin:admin -X GET http://$AMBARI_HOST:8080/api/v1/clusters |grep cluster_name|grep -Po ': "(.+)'|grep -Po '[a-zA-Z0-9\-_!?.]+')
 
-getZeppelinHost () {
-       	ZEPPELIN_MASTER_HOST=$(curl -u admin:admin -X GET http://$AMBARI_HOST:8080/api/v1/clusters/$CLUSTER_NAME/services/NIFI/components/ZEPPELIN_MASTER|grep "host_name"|grep -Po ': "([a-zA-Z0-9\-_!?.]+)'|grep -Po '([a-zA-Z0-9\-_!?.]+)')
+echo 'AMBARI-HOST='$AMBARI_HOST
+echo 'CLUSTER-NAME='$CLUSTER_NAME
 
-       	echo $ZEPPELIN_MASTER_HOST
+getZeppelinHost () {
+
+        export ZEPPELIN_MASTER_HOST=$(curl -u admin:admin -X GET http://$AMBARI_HOST:8080/api/v1/clusters/$CLUSTER_NAME/services/ZEPPELIN/components/ZEPPELIN_MASTER |grep "host_name"|grep -Po ': "([a-zA-Z0-9\-_!?.]+)' | grep -Po '([a-zA-Z0-9\-_!?.]+)')
+       	echo 'ZEPPELIN_MASTER_HOST='$ZEPPELIN_MASTER_HOST
 }
 
-export NIFI_HOST=$(getNifiHost)
-echo "export ZEPPELIN_MASTER_HOST=$NIFI_HOST" >> ~/.bash_profile
-
+export ZEPPELIN_MASTER_HOST
  
 getZeppelinNotebook() {
-                ZEPP_NOTEBOOK=$ROOT_PATH/Hackathon2017/zeppelin/SysLog-Model-Regression.json
-                echo $ZEPP_NOTEBOOK
+                export ZEPP_NOTEBOOK=~/Hackathon2017/zeppelin/SysLog-Model-Regression.json
+                echo 'NOTEBOOK_PATH='$ZEPP_NOTEBOOK
 }
 
-getZeppelinHost()
-getZeppelinNotebook()
+getZeppelinHost
+getZeppelinNotebook
 # Login to Zeppelin
-curl -i --data 'userName=admin&password=admin' -X POST http://ZEPPSERVER_HOST:9995/api/login
-#curl -i --data 'userName=admin&password=admin' -X POST http://172.26.230.144:9995/api/login
- 
+export SESSIONID=$(curl -i --data 'userName=admin&password=admin' -X POST http://$ZEPPELIN_MASTER_HOST:9995/api/login | grep -Po '"ticket":"[a-zA-Z0-9-]+' | grep -Po ':"([a-zA-Z0-9\-_!?.]+)' | grep -Po '[a-zA-Z0-9\-_!?.]+')
+export JSONVAL='$(cat /root/Hackathon2017/zeppelin/SysLog-Model-Regression.json)'
+echo 'SESSIONID:'$SESSIONID
+
 # Create a Notebook
-#curl -H 'Content-Type:application/json' -XPUT -d $ZEPP_NOTEBOOK http://$ZEPPSERVER_HOST:9995/api/notebook
+
+curl -i -b 'JSESSIONID=$SESSIONID; Path=/; HttpOnly' -H 'Content-Type:application/json' -X POST -d '{\"name\":\"I am REST API Note !!!\"}' http://$ZEPPELIN_MASTER_HOST:9995/api/notebook
+
+#curl -i -b 'JSESSIONID=$SESSIONID; Path=/; HttpOnly' -H 'Content-Type:application/json' -X POST -d $JSONVAL http://$ZEPPELIN_MASTER_HOST:9995/api/notebook
  
-#curl -H 'Content-Type:application/json' -XPUT -d SysLogModel-v1.json http://172.26.230.144:9995/api/notebook
  
 # Run all paragraphs
 #http://ZEPPSERVER_HOST:9995/api/notebook/job/[notebookId]
-        
