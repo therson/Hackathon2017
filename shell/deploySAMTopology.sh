533fee87ef0ae0721fb06ef5de9d8d86eb3f7a48
zoharsan <zoharsan@gmail.com>
2017-10-24 11:38:31 -0400

    Update deploySAMTopology.sh
    Added createSAMCluster()

--- shell/deploySAMTopology.sh
+++ shell/deploySAMTopology.sh
@@ -23,6 +23,14 @@ createStormView () {
 
 }
 
+createSAMCluster() {
+        #Import cluster
+        export CLUSTER_ID=$(curl -H "content-type:application/json" -X POST http://$AMBARI_HOST:7777/api/v1/catalog/clusters -d '{"name":"'$CLUSTER_NAME'","description":"Demo Cluster","ambariImportUrl":"http://'$AMBARI_HOST':8080/api/v1/clusters/'$CLUSTER_NAME'"}'| grep -Po '\"id\":([0-9]+)'|grep -Po '([0-9]+)')
+
+        #Import cluster config
+        curl -H "content-type:application/json" -X POST http://$AMBARI_HOST:7777/api/v1/catalog/cluster/import/ambari -d '{"clusterId":'$CLUSTER_ID',"ambariRestApiRootUrl":"http://'$AMBARI_HOST':8080/api/v1/clusters/'$CLUSTER_NAME'","password":"admin","username":"admin"}'
+}
+
 initializeSAMNamespace () {
 	#Initialize New Namespace
 	export NAMESPACE_ID=$(curl -H "content-type:application/json" -X POST http://$AMBARI_HOST:7777/api/v1/catalog/namespaces -d '{"name":"test1","description":"test1","streamingEngine":"STORM"}'| grep -Po '\"id\":([0-9]+)'|grep -Po '([0-9]+)')
