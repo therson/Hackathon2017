a808e29e3e11bae24bd8643bab50395c5eb2fb3b
oascofare <oascofare@users.noreply.github.com>
2017-10-26 12:29:17 -0400

    machine-log-demo-Install.sh modified

--- recipes/postinstall/machine-log-demo-Install.sh
+++ recipes/postinstall/machine-log-demo-Install.sh
@@ -3,18 +3,18 @@ echo "*********************************Install Machine log demo data ***********
 yum install -y wget
 yum install -y git
 git clone https://github.com/vakshorton/CloudBreakArtifacts
-
+ 
 echo "************ Installing HDF components ************************"
 chmod +x -R ./CloudBreakArtifacts/recipes/
 ./CloudBreakArtifacts/recipes/hdp-hdf-post-install.sh
-
 echo "************ Installing Demo  ************************"
 git clone https://github.com/oascofare/Hackathon2017
 cd Hackathon2017
 chmod +x -R ./shell
+./shell/configRsyslog.sh
 ./shell/pushSchemasToRegistry.sh
 ./shell/deploySAMTopology.sh
+echo "* * * * *  root  /root/Hackathon2017/shell/gensessions-ag.sh 100" >> /etc/crontab
+service  crond restart
 ./shell/deployNifiTemplate.sh
-sudo ./shell/configRsyslog.sh
-sudo ./shell/gensessions-ag.sh 1000
 
