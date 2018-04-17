b8a45730a3420fbb0742d570266f5ae9337f4e69
Taylor Herson <therson@hortonworks.com>
2018-04-17 15:20:55 -0400

    update links

--- recipes/postinstall/machine-log-demo-Install.sh
+++ recipes/postinstall/machine-log-demo-Install.sh
@@ -2,13 +2,13 @@
 echo "*********************************Install Machine log demo data *****************************"
 yum install -y wget
 yum install -y git
-git clone https://github.com/vakshorton/CloudBreakArtifacts
+git clone https://github.com/amanda010792/CloudBreakArtifacts
  
 echo "************ Installing HDF components ************************"
 chmod +x -R ./CloudBreakArtifacts/recipes/
 ./CloudBreakArtifacts/recipes/hdp-hdf-post-install.sh
 echo "************ Installing Demo  ************************"
-git clone https://github.com/oascofare/Hackathon2017
+git clone https://github.com/therson/Hackathon2017
 cd Hackathon2017
 chmod +x -R ./shell
 ./shell/configRsyslog.sh
